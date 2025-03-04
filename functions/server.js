ec39e557b5778899221dfc11431157d4e118ab16
Mohsin Irfan <mohsin377@gmail.com>
2025-03-05 03:19:11 +0500

    add netlify setup

--- functions/server.js
+++ functions/server.js
@@ -1,6 +1,7 @@
-const app = require('./src/app');
+const app = require('../src/app');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const serverless = require("serverless-http");
 
 // Load environment variables
 dotenv.config();
@@ -18,3 +19,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+module.exports.handler = serverless(app);

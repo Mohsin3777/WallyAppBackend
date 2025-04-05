6f2f955da7eb767d27fd6000902939378d3103ca
Mohsin Irfan <mohsin377@gmail.com>
2025-04-05 16:09:39 +0500

    a

--- server.js
+++ server.js
@@ -33,4 +33,12 @@ mongoose
   .catch((err) => console.error('MongoDB connection error:', err));
 
 // ✅ Export the app (instead of listening)
-module.exports = app;
+
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+// module.exports = app;
